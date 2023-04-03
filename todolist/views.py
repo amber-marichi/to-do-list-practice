d1709474124dca4bfa2dcb585c6d4e6670dfd7ec
Max Bilokur <bilmaxim@gmail.com>
2023-04-03 11:04:49 +0300

    index page view and template initial version

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from todolist.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    tasks = Task.objects.all()
+    task_count = Task.objects.count()
+
+    context = {
+        "task_count": task_count,
+        "tasks": tasks,
+    }
+
+    return render(request, "todolist/index.html", context)
