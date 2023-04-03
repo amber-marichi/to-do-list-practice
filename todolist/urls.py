04140121444818841d95649ef75cd08716b6f7b6
Max Bilokur <bilmaxim@gmail.com>
2023-04-03 12:25:52 +0300

    added form for tag, tag list redone into table

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 
-from todolist.views import index
+from todolist.views import (
+    index,
+    TagListView,
+    TagCreateView,
+)
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create"
+    )
 ]
 
 app_name = "todolist"
--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views import generic
+from django.urls import reverse_lazy
 
-from todolist.models import Task
+from todolist.models import Tag, Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by("is_done")
     task_count = Task.objects.count()
 
     context = {
@@ -14,3 +16,13 @@ def index(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, "todolist/index.html", context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:tag-list")
